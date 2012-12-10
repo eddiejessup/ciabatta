44418d31053c9182a7b577133f4ab559850eeb27
Elliot Marsden <elliot.marsden@gmail.com>
2012-12-10 17:05:59 +0000

    Making cython util funcs non-inline to stop warnings

--- Lib/utils_cy.pyx
+++ Lib/utils_cy.pyx
@@ -1,23 +1,23 @@
 cimport cython
 
-cdef inline double square(double f):
+cdef double square(double f):
     return f * f
 
-cdef inline double mag_sq(double x, double y):
+cdef double mag_sq(double x, double y):
     return square(x) + square(y)
 
-cdef inline double wrap_real(double L, double L_half, double r):
+cdef double wrap_real(double L, double L_half, double r):
     if r > L_half: return r - L
     elif r < -L_half: return r + L
     else: return r
 
-cdef inline unsigned int wrap(unsigned int M, int i):
+cdef unsigned int wrap(unsigned int M, int i):
     if i >= M: return i - M
     elif i < 0: return i + M
     else: return i
 
-cdef inline unsigned int wrap_inc(unsigned int M, unsigned int i):
+cdef unsigned int wrap_inc(unsigned int M, unsigned int i):
     return i + 1 if i < M - 1 else 0
 
-cdef inline unsigned int wrap_dec(unsigned int M, unsigned int i):
+cdef unsigned int wrap_dec(unsigned int M, unsigned int i):
     return i - 1 if i > 0 else M - 1
