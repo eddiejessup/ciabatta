345c89aaa6b1406f78a83aa8108eb79d7c63f738
Elliot Marsden <elliot.marsden@gmail.com>
2012-12-19 12:21:51 +0000

    Adding another interacts algorithm and improving existing

--- Lib/numerics.pyx
+++ Lib/numerics.pyx
@@ -1,64 +1,93 @@
-# cython: profile=True
-
+# cython: profile=False
 import numpy as np
-import fields
-import utils
 
 cimport numpy as np
 from utils_cy cimport square, wrap_inc, wrap_dec, wrap_real
 
-def interacts_cell_list(np.ndarray[np.float_t, ndim=2] r, double L, double R_cut):
-    cdef unsigned int i, i_2, i_dim
-    cdef double R_cut_sq = square(R_cut), L_half = L / 2.0, R_sep_sq
+cdef double R_sep_sq(double r_1_x, double r_1_y, double r_2_x, double r_2_y, double L, double L_half):
+    return square(wrap_real(L, L_half, r_1_x - r_2_x)) + square(wrap_real(L, L_half, r_1_y - r_2_y))
+
+def interacts_cl_nochecks(np.ndarray[np.float_t, ndim=2] r, double L, double R_cut):
+    cdef unsigned int M = int(L / R_cut), i, i_2, x, y, x_inc, x_dec, y_inc, y_dec
+    cdef double R_cut_sq = square(R_cut), L_half = L / 2.0
+    cdef list cell_list = [[[] for i_2 in range(M)] for i in range(M)]
     cdef list interacts = [[] for i in range(r.shape[0])]
 
-    # Cell list stuff
-    cdef unsigned int M = int(L / R_cut)
-    cdef list checks = [[] for i in range(r.shape[0])]
-    cdef list cell_list = utils.empty_lists(M, r.shape[1])
     cdef np.ndarray[np.int_t, ndim=2] inds = np.asarray((r + L_half) /  (L / float(M)), dtype=np.int)
 
-    if r.shape[1] == 1:
-        for i in range(r.shape[0]):
-            cell_list[inds[i, 0]].append(i)
+    for i in range(r.shape[0]):
+        cell_list[inds[i, 0]][inds[i, 1]].append(i)
 
-        for i in range(r.shape[0]):
-            i_x, = inds[i]
-            checks[i] += cell_list[i_x]
-            checks[i] += cell_list[wrap_inc(M, i_x)]
-            checks[i] += cell_list[wrap_dec(M, i_x)]
+    for x in range(M):
+        for y in range(M):
+            if not cell_list[x][y]: continue
+            x_inc = wrap_inc(M, x)
+            x_dec = wrap_dec(M, x)
+            y_inc = wrap_inc(M, y)
+            y_dec = wrap_dec(M, y)
+            for i in cell_list[x][y]:
+                for i_2 in cell_list[x][y]:
+                    if i_2 == i: continue
+                    if R_sep_sq(r[i, 0], r[i, 1], r[i_2, 0], r[i_2, 1], L, L_half) < R_cut_sq:
+                        interacts[i].append(i_2)
+                for i_2 in cell_list[x][y_inc]:
+                    if R_sep_sq(r[i, 0], r[i, 1], r[i_2, 0], r[i_2, 1], L, L_half) < R_cut_sq:                    
+                        interacts[i].append(i_2)
+                for i_2 in cell_list[x][y_dec]:
+                    if R_sep_sq(r[i, 0], r[i, 1], r[i_2, 0], r[i_2, 1], L, L_half) < R_cut_sq:
+                        interacts[i].append(i_2)
+                for i_2 in cell_list[x_inc][y]:
+                    if R_sep_sq(r[i, 0], r[i, 1], r[i_2, 0], r[i_2, 1], L, L_half) < R_cut_sq:
+                        interacts[i].append(i_2)
+                for i_2 in cell_list[x_dec][y]:
+                    if R_sep_sq(r[i, 0], r[i, 1], r[i_2, 0], r[i_2, 1], L, L_half) < R_cut_sq:
+                        interacts[i].append(i_2)
+                for i_2 in cell_list[x_inc][y_inc]:
+                    if R_sep_sq(r[i, 0], r[i, 1], r[i_2, 0], r[i_2, 1], L, L_half) < R_cut_sq:
+                        interacts[i].append(i_2)
+                for i_2 in cell_list[x_inc][y_dec]:
+                    if R_sep_sq(r[i, 0], r[i, 1], r[i_2, 0], r[i_2, 1], L, L_half) < R_cut_sq:
+                        interacts[i].append(i_2)
+                for i_2 in cell_list[x_dec][y_inc]:
+                    if R_sep_sq(r[i, 0], r[i, 1], r[i_2, 0], r[i_2, 1], L, L_half) < R_cut_sq:
+                        interacts[i].append(i_2)
+                for i_2 in cell_list[x_dec][y_dec]:
+                    if R_sep_sq(r[i, 0], r[i, 1], r[i_2, 0], r[i_2, 1], L, L_half) < R_cut_sq:
+                        interacts[i].append(i_2)
+    return interacts
 
-    elif r.shape[1] == 2:
-        for i in range(r.shape[0]):
-            cell_list[inds[i, 0]][inds[i, 1]].append(i)
+def interacts_cl_checks(np.ndarray[np.float_t, ndim=2] r, double L, double R_cut):
+    cdef unsigned int M = int(L / R_cut), i, i_2, x, y, x_inc, x_dec, y_inc, y_dec
+    cdef double R_cut_sq = square(R_cut), L_half = L / 2.0
+    cdef list cell_list = [[[] for i_2 in range(M)] for i in range(M)]
+    cdef list checks = [[] for i in range(r.shape[0])]
+    cdef list interacts = [[] for i in range(r.shape[0])]
 
-        for i in range(r.shape[0]):
-            i_x, i_y = inds[i]
-            checks[i] += cell_list[i_x][i_y]
-            checks[i] += cell_list[wrap_inc(M, i_x)][i_y]
-            checks[i] += cell_list[wrap_dec(M, i_x)][i_y]
-            checks[i] += cell_list[i_x][wrap_inc(M, i_y)]
-            checks[i] += cell_list[i_x][wrap_dec(M, i_y)]
-            checks[i] += cell_list[wrap_inc(M, i_x)][wrap_inc(M, i_y)]
-            checks[i] += cell_list[wrap_inc(M, i_x)][wrap_dec(M, i_y)]
-            checks[i] += cell_list[wrap_dec(M, i_x)][wrap_inc(M, i_y)]
-            checks[i] += cell_list[wrap_dec(M, i_x)][wrap_dec(M, i_y)]
-
-    elif r.shape[1] == 3:
-        for i in range(r.shape[0]):
-            cell_list[inds[i, 0]][inds[i, 1]][inds[i, 2]].append(i)
-        raise NotImplementedError
-
-    else:
-        raise Exception('Cell list not implemented in this dimension')
+    cdef np.ndarray[np.int_t, ndim=2] inds = np.asarray((r + L_half) /  (L / float(M)), dtype=np.int)
 
     for i in range(r.shape[0]):
-        checks[i].remove(i)       
+        cell_list[inds[i, 0]][inds[i, 1]].append(i)
+
+    for i in range(r.shape[0]):
+        x, y = inds[i]
+        x_inc = wrap_inc(M, x)
+        x_dec = wrap_dec(M, x)
+        y_inc = wrap_inc(M, y)
+        y_dec = wrap_dec(M, y)        
+        checks[i] += cell_list[x][y]
+        checks[i] += cell_list[x][y_inc]
+        checks[i] += cell_list[x][y_dec]
+        checks[i] += cell_list[x_inc][y]
+        checks[i] += cell_list[x_dec][y]
+        checks[i] += cell_list[x_inc][y_inc]
+        checks[i] += cell_list[x_inc][y_dec]
+        checks[i] += cell_list[x_dec][y_inc]
+        checks[i] += cell_list[x_dec][y_dec]
+
+    for i in range(r.shape[0]):
         for i_2 in checks[i]:
-            R_sep_sq = 0.0
-            for i_dim in range(r.shape[1]):
-                R_sep_sq += square(wrap_real(L, L_half, r[i, i_dim] - r[i_2, i_dim]))
-            if R_sep_sq < R_cut_sq:
+            if i_2 == i: continue
+            if R_sep_sq(r[i, 0], r[i, 1], r[i_2, 0], r[i_2, 1], L, L_half) < R_cut_sq:
                 interacts[i].append(i_2)
     return interacts
 
