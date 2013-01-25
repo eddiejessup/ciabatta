4d02e1f3e25983205bd1dc691653654d897e5c24
Elliot Marsden <elliot.marsden@gmail.com>
2013-01-25 00:40:17 +0000

    Making python cell list module cythonic

--- Lib/setup.py
+++ Lib/setup.py
@@ -12,6 +12,7 @@ setup(
     ext_modules = [
         Extension("field_numerics", ["field_numerics.pyx",]),
         Extension("walled_field_numerics", ["walled_field_numerics.pyx",]),
+        Extension("cl", ["cl.pyx",]),
     ],
     include_dirs = [numpy.get_include()],
 )
