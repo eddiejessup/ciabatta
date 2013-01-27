9e7b5fc2ccbf1f7bb4fc054518f7f9f57d2058d3
Elliot Marsden <elliot.marsden@gmail.com>
2013-01-27 19:10:55 +0000

    Adding simple mail script

---
+++ Scripts/mail.py
@@ -0,0 +1,22 @@
+#! /usr/bin/python
+
+import smtplib
+
+username = 'elliot.marsden'
+password = raw_input('Password: ')
+recipient = raw_input('To: ')
+subject = raw_input('Subject: ')
+body = raw_input('Body: ')
+
+headers = '\r\n'.join([
+    'from: %s' % username,
+    'subject: %s' % subject,
+    'to: %s' % recipient,
+    'mime-version: 1.0',
+    'content-type: text/html'])
+
+session = smtplib.SMTP('smtp.gmail.com', 587)
+session.ehlo()
+session.starttls()
+session.login(username, password)
+session.sendmail(username, recipient, headers + '\r\n\r\n' + body)
