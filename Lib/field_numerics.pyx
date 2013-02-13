2393e3fb0759adbca9820e2ab6a2d3f8c7ab956d
Elliot Marsden <elliot.marsden@gmail.com>
2013-02-13 22:54:06 +0000

    Trivial changes

--- Lib/field_numerics.pyx
+++ Lib/field_numerics.pyx
@@ -10,14 +10,14 @@ cdef unsigned int wrap_dec(unsigned int M, unsigned int i):
 
 def div(field, dx):
     assert dx > 0.0
-    div = np.empty(field.shape[:-1], dtype=field.dtype)    
+    div = np.empty(field.shape[:-1], dtype=field.dtype)
     if field.ndim == 2: div_1d(field, div, dx)
     elif field.ndim == 3: div_2d(field, div, dx)
     elif field.ndim == 4: div_3d(field, div, dx)
     else: raise Exception('Divergence not implemented in this dimension')
     return div
 
-def div_1d(np.ndarray[np.float_t, ndim=2] field, 
+def div_1d(np.ndarray[np.float_t, ndim=2] field,
         np.ndarray[np.float_t, ndim=1] div,
         double dx):
     cdef unsigned int i_x
@@ -27,7 +27,7 @@ def div_1d(np.ndarray[np.float_t, ndim=2] field,
     for i_x in range(M_x):
         div[i_x] = (field[wrap_inc(M_x, i_x), 0] - field[wrap_dec(M_x, i_x), 0]) / dx_double
 
-def div_2d(np.ndarray[np.float_t, ndim=3] field, 
+def div_2d(np.ndarray[np.float_t, ndim=3] field,
         np.ndarray[np.float_t, ndim=2] div,
         double dx):
     cdef unsigned int i_x, i_y
@@ -37,11 +37,11 @@ def div_2d(np.ndarray[np.float_t, ndim=3] field,
     for i_x in range(M_x):
         for i_y in range(M_y):
             div[i_x, i_y] = (
-                (field[wrap_inc(M_x, i_x), i_y, 0] - field[wrap_dec(M_x, i_x), i_y, 0]) + 
-                (field[i_x, wrap_inc(M_y, i_y), 1] - field[i_x, wrap_dec(M_y, i_y), 1]) + 
+                (field[wrap_inc(M_x, i_x), i_y, 0] - field[wrap_dec(M_x, i_x), i_y, 0]) +
+                (field[i_x, wrap_inc(M_y, i_y), 1] - field[i_x, wrap_dec(M_y, i_y), 1]) +
                 (field[wrap_inc(M_x, i_x), i_y, 0] - field[wrap_dec(M_x, i_x), i_y, 0])) / dx_double
 
-def div_3d(np.ndarray[np.float_t, ndim=4] field, 
+def div_3d(np.ndarray[np.float_t, ndim=4] field,
         np.ndarray[np.float_t, ndim=3] div,
         double dx):
     cdef unsigned int i_x, i_y, i_z
@@ -53,8 +53,8 @@ def div_3d(np.ndarray[np.float_t, ndim=4] field,
         for i_y in range(M_y):
             for i_z in range(M_z):
                 div[i_x, i_y, i_z] = (
-                    (field[wrap_inc(M_x, i_x), i_y, i_z, 0] - field[wrap_dec(M_x, i_x), i_y, i_z, 0]) + 
-                    (field[i_x, wrap_inc(M_y, i_y), i_z, 1] - field[i_x, wrap_dec(M_y, i_y), i_z, 1]) + 
+                    (field[wrap_inc(M_x, i_x), i_y, i_z, 0] - field[wrap_dec(M_x, i_x), i_y, i_z, 0]) +
+                    (field[i_x, wrap_inc(M_y, i_y), i_z, 1] - field[i_x, wrap_dec(M_y, i_y), i_z, 1]) +
                     (field[i_x, i_y, wrap_inc(M_z, i_z), 2] - field[i_x, i_y, wrap_dec(M_z, i_z), 2])) / dx_double
 
 def grad(field, dx):
@@ -66,7 +66,7 @@ def grad(field, dx):
     else: raise Exception('Grad not implemented in this dimension')
     return grad
 
-def grad_1d(np.ndarray[np.float_t, ndim=1] field, 
+def grad_1d(np.ndarray[np.float_t, ndim=1] field,
         np.ndarray[np.float_t, ndim=2] grad,
         double dx):
     cdef unsigned int i_x
@@ -76,7 +76,7 @@ def grad_1d(np.ndarray[np.float_t, ndim=1] field,
     for i_x in range(M_x):
         grad[i_x, 0] = (field[wrap_inc(M_x, i_x)] - field[wrap_dec(M_x, i_x)]) / dx_double
 
-def grad_2d(np.ndarray[np.float_t, ndim=2] field, 
+def grad_2d(np.ndarray[np.float_t, ndim=2] field,
         np.ndarray[np.float_t, ndim=3] grad,
         double dx):
     cdef unsigned int i_x, i_y
@@ -88,7 +88,7 @@ def grad_2d(np.ndarray[np.float_t, ndim=2] field,
             grad[i_x, i_y, 0] = (field[wrap_inc(M_x, i_x), i_y] - field[wrap_dec(M_x, i_x), i_y]) / dx_double
             grad[i_x, i_y, 1] = (field[i_x, wrap_inc(M_y, i_y)] - field[i_x, wrap_dec(M_y, i_y)]) / dx_double
 
-def grad_3d(np.ndarray[np.float_t, ndim=3] field, 
+def grad_3d(np.ndarray[np.float_t, ndim=3] field,
         np.ndarray[np.float_t, ndim=4] grad,
         double dx):
     cdef unsigned int i_x, i_y, i_z
@@ -114,7 +114,7 @@ def grad_i(field, inds, dx):
     else: raise Exception("Grad_i not implemented in this dimension")
     return grad_i
 
-def grad_i_1d(np.ndarray[np.float_t, ndim=1] field, 
+def grad_i_1d(np.ndarray[np.float_t, ndim=1] field,
         np.ndarray[np.int_t, ndim=2] inds,
         np.ndarray[np.float_t, ndim=2] grad_i,
         double dx):
@@ -126,7 +126,7 @@ def grad_i_1d(np.ndarray[np.float_t, ndim=1] field,
         i_x = inds[i, 0]
         grad_i[i, 0] = (field[wrap_inc(M_x, i_x)] - field[wrap_dec(M_x, i_x)]) / dx_double
 
-def grad_i_2d(np.ndarray[np.float_t, ndim=2] field, 
+def grad_i_2d(np.ndarray[np.float_t, ndim=2] field,
         np.ndarray[np.int_t, ndim=2] inds,
         np.ndarray[np.float_t, ndim=2] grad_i,
         double dx):
@@ -139,7 +139,7 @@ def grad_i_2d(np.ndarray[np.float_t, ndim=2] field,
         grad_i[i, 0] = (field[wrap_inc(M_x, i_x), i_y] - field[wrap_dec(M_x, i_x), i_y]) / dx_double
         grad_i[i, 1] = (field[i_x, wrap_inc(M_y, i_y)] - field[i_x, wrap_dec(M_y, i_y)]) / dx_double
 
-def grad_i_3d(np.ndarray[np.float_t, ndim=3] field, 
+def grad_i_3d(np.ndarray[np.float_t, ndim=3] field,
         np.ndarray[np.int_t, ndim=2] inds,
         np.ndarray[np.float_t, ndim=2] grad_i,
         double dx):
@@ -161,9 +161,9 @@ def laplace(field, dx):
     elif field.ndim == 2: laplace_2d(field, laplace, dx)
     elif field.ndim == 3: laplace_3d(field, laplace, dx)
     else: raise Exception('Laplacian not implemented in this dimension')
-    return laplace 
+    return laplace
 
-def laplace_1d(np.ndarray[np.float_t, ndim=1] field, 
+def laplace_1d(np.ndarray[np.float_t, ndim=1] field,
         np.ndarray[np.float_t, ndim=1] laplace,
         double dx):
     cdef unsigned int i_x
@@ -172,10 +172,10 @@ def laplace_1d(np.ndarray[np.float_t, ndim=1] field,
 
     for i_x in range(M_x):
         laplace[i_x] = (
-            field[wrap_inc(M_x, i_x)] + field[wrap_dec(M_x, i_x)] - 
+            field[wrap_inc(M_x, i_x)] + field[wrap_dec(M_x, i_x)] -
             2.0 * field[i_x]) / dx_sq
 
-def laplace_2d(np.ndarray[np.float_t, ndim=2] field, 
+def laplace_2d(np.ndarray[np.float_t, ndim=2] field,
         np.ndarray[np.float_t, ndim=2] laplace,
         double dx):
     cdef unsigned int i_x, i_y
@@ -185,11 +185,11 @@ def laplace_2d(np.ndarray[np.float_t, ndim=2] field,
     for i_x in range(M_x):
         for i_y in range(M_y):
             laplace[i_x, i_y] = (
-                field[wrap_inc(M_x, i_x), i_y] + field[wrap_dec(M_x, i_x), i_y] + 
-                field[i_x, wrap_inc(M_y, i_y)] + field[i_x, wrap_dec(M_y, i_y)] - 
+                field[wrap_inc(M_x, i_x), i_y] + field[wrap_dec(M_x, i_x), i_y] +
+                field[i_x, wrap_inc(M_y, i_y)] + field[i_x, wrap_dec(M_y, i_y)] -
                 4.0 * field[i_x, i_y]) / dx_sq
 
-def laplace_3d(np.ndarray[np.float_t, ndim=3] field, 
+def laplace_3d(np.ndarray[np.float_t, ndim=3] field,
         np.ndarray[np.float_t, ndim=3] laplace,
         double dx):
     cdef unsigned int i_x, i_y, i_z
@@ -202,15 +202,16 @@ def laplace_3d(np.ndarray[np.float_t, ndim=3] field,
             for i_z in range(M_z):
                 laplace[i_x, i_y, i_z] = (
                     field[wrap_inc(M_x, i_x), i_y, i_z] + field[wrap_dec(M_x, i_x), i_y, i_z] +
-                    field[i_x, wrap_inc(M_y, i_y), i_z] + field[i_x, wrap_dec(M_y, i_y), i_z] + 
-                    field[i_x, i_y, wrap_inc(M_z, i_z)] + field[i_x, i_y, wrap_dec(M_z, i_z)] - 
+                    field[i_x, wrap_inc(M_y, i_y), i_z] + field[i_x, wrap_dec(M_y, i_y), i_z] +
+                    field[i_x, i_y, wrap_inc(M_z, i_z)] + field[i_x, i_y, wrap_dec(M_z, i_z)] -
                     6.0 * field[i_x, i_y, i_z]) / dx_sq
 
 def density(r, L, dx):
     assert r.ndim == 2
-    if (L / dx) % 1 != 0:
-        raise Exception
-    M = int(L / dx)
+#    if (L / dx) % 1 != 0:
+ #       raise Warning('requested density field spatial step not compatible with system size')
+    M = int(round(L / dx))
+    dx = L / M
     inds = utils.r_to_i(r, L, dx)
     f = np.zeros(r.shape[1] * (M,), dtype=np.int)
     if f.ndim == 1: density_1d(inds, f)
@@ -219,19 +220,19 @@ def density(r, L, dx):
     else: raise Exception('Density calc not implemented in this dimension')
     return f / dx ** r.shape[1]
 
-def density_1d(np.ndarray[np.int_t, ndim=2] inds, 
+def density_1d(np.ndarray[np.int_t, ndim=2] inds,
         np.ndarray[np.int_t, ndim=1] f):
     cdef unsigned int i_part
     for i_part in range(inds.shape[0]):
         f[inds[i_part, 0]] += 1
 
-def density_2d(np.ndarray[np.int_t, ndim=2] inds, 
+def density_2d(np.ndarray[np.int_t, ndim=2] inds,
         np.ndarray[np.int_t, ndim=2] f):
     cdef unsigned int i_part
     for i_part in range(inds.shape[0]):
         f[inds[i_part, 0], inds[i_part, 1]] += 1
 
-def density_3d(np.ndarray[np.int_t, ndim=2] inds, 
+def density_3d(np.ndarray[np.int_t, ndim=2] inds,
         np.ndarray[np.int_t, ndim=3] f):
     cdef unsigned int i_part
     for i_part in range(inds.shape[0]):
