486cfdd90b680d1a5579b00fad901b8c8f0f240c
Elliot Marsden <elliot.marsden@gmail.com>
2013-02-26 16:42:09 +0000

    Updating and improving rotational diffusion test funcs

--- Lib/test/test_rot_diff.py
+++ Lib/test/test_rot_diff.py
@@ -3,38 +3,32 @@ import utils
 import matplotlib.pyplot as pp
 from mpl_toolkits.mplot3d import Axes3D
 
+dt = 0.01
+n = 10
+
 def D_dependence(d=3):
     samples = 400
+
     Ds_calc = []
     Ds_in = np.linspace(0.00001, 1.0, num=samples)
-    n = 1000
-    dt = 0.1
-
-    if d == 2: rot_diff = utils.rot_diff_2d
-    elif d == 3: rot_diff = utils.rot_diff_3d
-
-    for D_rot in Ds_in:
+    for D_rot in Ds_in  :
         a = utils.sphere_pick(d, n)
-        a_rot = rot_diff(a, D_rot, dt)
-        dthetas = utils.vector_angle(a, a_rot)
-        D_rot_calc = (dthetas ** 2).mean() / (2.0 * dt)
-        Ds_calc.append(D_rot_calc)
+        a_rot = utils.rot_diff(a, D_rot, dt)
+        Ds_calc.append(utils.calc_D_rot(a, a_rot, dt))
 
     pp.scatter(Ds_in, np.array(Ds_calc) / Ds_in)
     pp.show()
 
 def diffusion_track(d):
-    dt = 0.1
     D_rot = 0.01
-    n = 1000
-    a_rots = np.zeros([n, d])
+    t = 20.0
+    iters = int(round(t / dt))
+
+    a_rots = np.zeros([iters, d])
     a_rots[0] = utils.sphere_pick(d, 1)
 
-    if d == 2: rot_diff = utils.rot_diff_2d
-    elif d == 3: rot_diff = utils.rot_diff_3d
-
-    for i in range(1, n):
-        a_rots[i] = rot_diff(np.array([a_rots[i - 1]]), D_rot, dt)
+    for i in range(1, iters):
+        a_rots[i] = utils.rot_diff(np.array([a_rots[i - 1]]), D_rot, dt)
 
     fig = pp.figure()
     if d == 2:
@@ -45,8 +39,8 @@ def diffusion_track(d):
         ax.set_ylim([-r, r])
     elif d == 3:
         ax = fig.add_subplot(111, projection='3d')
-        u = np.linspace(0, 2 * np.pi, 100)
-        v = np.linspace(0, np.pi, 100)
+        u = np.linspace(0, 2 * np.pi, 60)
+        v = np.linspace(0, np.pi, 60)
         x = np.outer(np.cos(u), np.sin(v))
         y = np.outer(np.sin(u), np.sin(v))
         z = np.outer(np.ones(np.size(u)), np.cos(v))
@@ -57,5 +51,5 @@ def diffusion_track(d):
     pp.show()
 
 if __name__ == '__main__':
-    D_dependence(2)
+    D_dependence(3)
     diffusion_track(3)
