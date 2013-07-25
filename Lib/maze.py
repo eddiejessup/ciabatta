d5c1245bcbdbc4e5d1849ae2588755af0b150ba7
Elliot Marsden <elliot.marsden@gmail.com>
2013-07-25 01:35:51 +0100

    Add maze shrink utility function

--- Lib/maze.py
+++ Lib/maze.py
@@ -7,6 +7,29 @@ from __future__ import print_function
 import numpy as np
 import utils
 
+def shrink(w_old, n):
+    if n < 1: raise Exception('Shrink factor >= 1')
+    elif n == 1: return w_old
+    elif n % 2 != 0: raise Exception('Shrink factor must be odd')
+    M = w_old.shape[0]
+    w_new = np.zeros(w_old.ndim * [M * n], dtype=w_old.dtype)
+    mid = n // 2
+    for x in range(M):
+        x_ = x * n
+        for y in range(M):
+            y_ = y * n
+            if w_old[x, y]:
+                w_new[x_ + mid, y_ + mid] = True
+                if w_old[utils.wrap_inc(M, x), y]:
+                    w_new[x_ + mid:x_ + n, y_ + mid] = True
+                if w_old[utils.wrap_dec(M, x), y]:
+                    w_new[x_:x_ + mid, y_ + mid] = True
+                if w_old[x, utils.wrap_inc(M, y)]:
+                    w_new[x_ + mid, y_ + mid:y_ + n] = True
+                if w_old[x, utils.wrap_dec(M, y)]:
+                    w_new[x_ + mid, y * n:y_ + mid] = True
+    return w_new
+
 def step(p, o, m, n=1):
     p_new = np.array(p) + n * np.array(o)
     p_new[p_new < 0] += m
