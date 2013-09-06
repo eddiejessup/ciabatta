7ede70c94ce992b8c158e887c8d0dd11535ec5b6
Elliot Marsden <elliot.marsden@gmail.com>
2013-09-06 17:07:30 +0100

    Rewrite maze to use more numpy arrays

--- Lib/maze.py
+++ Lib/maze.py
@@ -30,51 +30,80 @@ def shrink(w_old, n):
                     w_new[x_ + mid, y * n:y_ + mid] = True
     return w_new
 
+def make_offsets(dim):
+    offsets = np.zeros([2 * dim, dim])
+    offsets[:dim] = np.identity(dim)
+    offsets[dim:] = -np.identity(dim)
+    return offsets
+
+# def step(p, o, m, n=1):
+#     p_new = p + n * o
+#     p_new[p_new < 0] += m
+#     p_new[p_new > m - 1] -= m
+#     return p_new
+
+# def make_maze_dfs(M=27, dim=2, seed=None):
+#     ''' Generate a maze using the depth first search algorithm '''
+#     if M % 2 != 0:
+#         raise Exception('Require Maze size to be even.')
+#     rng = np.random.RandomState(seed)
+#     maze = np.zeros(dim * (M,), dtype=np.bool)
+#     pos = rng.randint(0, M, dim)
+#     maze[tuple(pos)] = True
+#     path = [pos]
+#     offsets = make_offsets(dim)
+#     while path:
+#         neighbs = []
+#         for offset in offsets:
+#             if not maze[tuple(step(pos, offset, M, 2))]:
+#                 neighbs.append(offset)
+#         if neighbs:
+#             print(neighbs)
+#             offset = neighbs[rng.randint(len(neighbs))]
+#             for i in range(2):
+#                 pos = step(pos, offset, M)
+#                 maze[tuple(pos)] = True
+#             path.append(pos)
+#         else:
+#             pos = path.pop()
+#     return maze
+
 def step(p, o, m, n=1):
-    p_new = np.array(p) + n * np.array(o)
+    p_new = p + n * o
     p_new[p_new < 0] += m
     p_new[p_new > m - 1] -= m
-    return tuple(p_new)
-
-def make_offsets(dim):
-    offsets = []
-    for d in range(dim):
-        for sign in (-1, +1):
-            offset = dim * [0]
-            offset[d] = sign
-            offsets.append(offset)
-    return tuple(offsets)
+    return p_new
 
 def make_maze_dfs(M=27, dim=2, seed=None):
     ''' Generate a maze using the depth first search algorithm '''
-    if M <= 1:
-        raise Exception('Require Maze size > 1.')
-    if M % 2 != 0:
-        raise Exception('Require Maze size to be even.')
+    # if M % 2 != 0:
+    #     raise Exception('Require Maze size to be even.')
     rng = np.random.RandomState(seed)
     maze = np.zeros(dim * (M,), dtype=np.bool)
-    pos = tuple(rng.randint(0, M, dim))
-    maze[pos] = True
+    pos = rng.randint(0, M, dim)
+    maze[tuple(pos)] = True
     path = [pos]
+    visiteds = [tuple(pos)]
     offsets = make_offsets(dim)
-    while len(path) > 0:
+    while path:
         neighbs = []
         for offset in offsets:
-            if not maze[step(pos, offset, M, 2)]:
-                neighbs.append(offset)
-        if len(neighbs) > 0:
-            offset = neighbs[rng.randint(len(neighbs))]
-            pos = step(pos, offset, M)
-            maze[pos] = True
-            pos = step(pos, offset, M)
-            maze[pos] = True
+            p_new = step(pos, offset, M)
+            if tuple(p_new) not in visiteds:
+                neighbs.append(p_new)
+        if neighbs:
+            pos = neighbs[rng.randint(len(neighbs))]
+            maze[tuple(pos)] = True
             path.append(pos)
+            visiteds.append(tuple(pos))
         else:
             pos = path.pop()
+    print(visiteds)
     return maze
 
 def main():
-    maze = make_maze_dfs(30)
+    maze = make_maze_dfs(29)
+    print(maze[0,0])
     import matplotlib.pyplot as pp
     pp.imshow(maze, interpolation='nearest')
     pp.show()
