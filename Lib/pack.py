e9b2784024e865ecd0fcdc4d6f6816f9992c19b0
Elliot Marsden <elliot.marsden@gmail.com>
2014-07-06 16:21:08 +0100

    Make pack module more robust and easier to use

--- Lib/pack.py
+++ Lib/pack.py
@@ -1,30 +1,27 @@
+'''
+Pack spheres into periodic systems.
+'''
+
 import numpy as np
-import matplotlib.pyplot as pp
-import utils
 import geom
+from periodic import pdist_periodic, cdist_periodic
+import matplotlib.pyplot as plt
 
 
 class MetroRCP(object):
 
-    def __init__(self, r_0, V_0, R, dr_max, dV_max):
+    def __init__(self, r_0, L_0, R, dr_max, dL_max):
         self.r = r_0
         self.n, self.dim = self.r.shape
-        self.L = V_0 ** (1.0 / self.dim)
+        self.L = L_0
         self.R = R
-        self.D_sq = (2.0 * self.R) ** 2
         self.dr_max = dr_max
-        self.dV_max = dV_max
+        self.dL_max = dL_max
 
-        self.V_full = self.n * geom.sphere_volume(self.R, self.dim)
-
-        self.r_diff_sq = utils.vector_mag_sq(
-            self.r[np.newaxis, :] - self.r[:, np.newaxis])
-        self.r_diff_sq[np.identity(self.n, dtype=np.bool)] = np.inf
+        self.sep_sq = pdist_periodic(r_0, self.L)
 
     def U(self):
-        if np.any(np.abs(self.r) > self.L / 2.0 - self.R):
-            return np.inf
-        if np.any(self.r_diff_sq < self.D_sq):
+        if np.any(self.sep_sq < (2.0 * self.R) ** 2):
             return np.inf
         return 1.0 / self.pf()
 
@@ -32,35 +29,37 @@ class MetroRCP(object):
         self.i = np.random.randint(self.n)
         self.r_old = self.r[self.i].copy()
 
-        self.r[self.i] += np.random.uniform(
-            -self.dr_max * self.L, self.dr_max * self.L, self.dim)
+        dr = np.random.uniform(-self.dr_max * self.L,
+                               self.dr_max * self.L, self.dim)
+        self.r[self.i] += dr
+        self.r[self.r > self.L / 2.0] -= self.L
+        self.r[self.r < -self.L / 2.0] += self.L
 
-        self.r_diff_sq[self.i, :] = self.r_diff_sq[
-            :, self.i] = utils.vector_mag_sq(self.r[self.i] - self.r)
-        self.r_diff_sq[self.i, self.i] = np.inf
+        self.sep_sq_old = self.sep_sq[self.i].copy()
+
+        sep_sq = cdist_periodic(self.r[np.newaxis, self.i], self.r, self.L)
+
+        self.sep_sq[self.i, :] = sep_sq
+        self.sep_sq[:, self.i] = sep_sq
+        self.sep_sq[self.i, self.i] = np.inf
 
     def revert_r(self):
         self.r[self.i] = self.r_old.copy()
 
-        self.r_diff_sq[self.i, :] = self.r_diff_sq[
-            :, self.i] = utils.vector_mag_sq(self.r[self.i] - self.r)
-        self.r_diff_sq[self.i, self.i] = np.inf
+        self.sep_sq[self.i, :] = self.sep_sq_old.copy()
+        self.sep_sq[:, self.i] = self.sep_sq_old.copy()
 
-    def displace_V(self):
-        self.L_old = self.L
+    def displace_L(self):
+        self.dL = 1.0 + np.random.uniform(-self.dL_max, self.dL_max)
 
-        V_new = self.V() * (1.0 + np.random.uniform(-self.dV_max, self.dV_max))
-        self.L = V_new ** (1.0 / self.dim)
-        self.r *= self.L / self.L_old
+        self.L *= self.dL
+        self.r *= self.dL
+        self.sep_sq *= self.dL ** 2
 
-        self.r_diff_sq *= (self.L / self.L_old) ** 2
-
-    def revert_V(self):
-        self.r /= self.L / self.L_old
-
-        self.r_diff_sq /= (self.L / self.L_old) ** 2
-
-        self.L = self.L_old
+    def revert_L(self):
+        self.L /= self.dL
+        self.r /= self.dL
+        self.sep_sq /= self.dL ** 2
 
     def iterate(self, beta):
         U_0 = self.U()
@@ -70,8 +69,8 @@ class MetroRCP(object):
             self.displace_r()
             revert = self.revert_r
         else:
-            self.displace_V()
-            revert = self.revert_V
+            self.displace_L()
+            revert = self.revert_L
 
         U_new = self.U()
         if np.exp(-beta * (U_new - U_0)) < np.random.uniform():
@@ -80,78 +79,120 @@ class MetroRCP(object):
     def V(self):
         return self.L ** self.dim
 
+    def V_full(self):
+        return self.n * geom.sphere_volume(self.R, self.dim)
+
     def pf(self):
-        return self.V_full / self.V()
+        return self.V_full() / self.V()
 
 
-def random_base(n, V, dim, R):
-    L = V ** (1.0 / dim)
-    r = np.zeros([n, dim], dtype=np.float)
-    lim = L / 2.0 - R
-    for i in range(n):
-        print('Initial packing: %.1f%% done' % ((i * 100.0) / n))
-        while True:
-            r[i] = np.random.uniform(-lim, lim, dim)
-            r_diff_sq = utils.vector_mag_sq(
-                r[:i + 1, np.newaxis] - r[np.newaxis, :i + 1])
-            if i == 0 or r_diff_sq[r_diff_sq > 0.0].min() > (2.0 * R) ** 2:
-                break
-    return r
+def n_to_pf(L, d, n, R):
+    '''
+    Packing fraction of n spheres
+    in d dimensions
+    of radius R
+    in a system of length L.
+    '''
+    return (n * geom.sphere_volume(R=R, n=d)) / L ** d
 
 
-def random_simple(pf, dim, R):
-    n = int(round(pf / geom.sphere_volume(R, dim)))
-    return random_base(n, dim, 1.0, R)
+def pf_to_n(L, d, pf, R):
+    '''
+    Number of spheres of radius R
+    to achieve as close to packing fraction pf as possible
+    in d dimensions
+    in a system of length L.
+    Also return the actual achieved packing fraction.
+    '''
+    n = int(round(pf * L ** d / geom.sphere_volume(R, d)))
+    pf_actual = n_to_pf(L, d, n, R)
+    return n, pf_actual
 
-every = 5000
+
+def calc_L_0(n, d, pf, R):
+    '''
+    Calculate system size required such that
+    for n spheres of radius R in d dimensions,
+    the packing fraction is pf.
+    Useful to initialise Metropolis algorithm to a reasonable state.
+    '''
+    return ((n * geom.sphere_volume(R=R, n=d)) / pf) ** (1.0 / d)
 
 
-def random(pf, dim, R, beta_max=1e4, V_0=2.0, dV_max=0.02, dr_max=0.02):
-    n = int(round(pf / geom.sphere_volume(R, dim)))
-    r_0 = random_base(n, V_0, dim, R)
-    mg = MetroRCP(r_0, V_0, R, dr_max, dV_max)
+def pack_simple(L, d,
+                n, R, seed):
+    '''
+    Pack n spheres into a system
+    of length L, dimension d,
+    using random seed,
+    using naïve uniform distribution of spheres,
+    and the tabula rasa rule.
+    (Start over from scratch if overlapping occurs.)
+    '''
+    np.random.seed(seed)
+    while True:
+        r = np.random.uniform(-L / 2.0, L / 2.0, size=(n, d))
+        if not np.any(pdist_periodic(r, L) < (2.0 * R) ** 2):
+            return r
+
+every = 500
+
+
+def draw_medium(r, R, L, ax=None):
+    if ax is None:
+        ax = plt.gca()
+    for x, y in r:
+        for ix in [-1, 0, 1]:
+            for iy in [-1, 0, 1]:
+                c = plt.Circle((x + ix*L, y + iy*L), radius=R, alpha=0.2)
+                ax.add_artist(c)
+    ax.set_aspect('equal')
+    ax.set_xlim(-L / 2.0, L / 2.0)
+    ax.set_ylim(-L / 2.0, L / 2.0)
+
+
+def pack(dim, R, beta_max=1e4, dL_max=0.02, dr_max=0.02,
+         seed=None, pf=None, n=None):
+    '''
+    Create a configuration of packed spheres
+    with radius R (expressed as a fraction of the system size).
+    in a periodic system of dimension dim.
+
+    Sphere packing can be specified by volume fraction vf,
+    or number of spheres n.
+
+    Configuration is created through the Metropolis-Hastings
+    algorithm for an NPT system.
+    '''
+
+    if pf is not None:
+        # If packing fraction is specified, find required number of spheres
+        # and the actual packing fraction this will produce
+        n, pf_actual = pf_to_n(1.0, dim, pf, R)
+    else:
+        # If n is specified, find packing fraction
+        pf_actual = n_to_pf(1.0, dim, n, R)
+
+    # Calculate an initial packing fraction and system size
+    # Start at at most 5%; lower if the desired packing fraction is very low
+    pf_initial = min(0.05, pf_actual / 2.0)
+    # Find system size that will create this packing fraction
+    L_0 = calc_L_0(n, dim, pf_initial, R)
+
+    # Pack naïvely into this system
+    r_0 = pack_simple(L_0, dim, n, R, seed)
+
+    mg = MetroRCP(r_0, L_0, R, dr_max, dL_max)
 
     t = 0
-    while mg.pf() < pf:
+    while mg.pf() < pf_actual:
         t += 1
         beta = beta_max * mg.pf()
         mg.iterate(beta)
 
-        if not t % every:
-            print('Packing: %.1f%%' % (100.0 * mg.pf()))
+        # if not t % every:
+        #     print('Packing: {:.1f}%'.format(100.0 * mg.pf()))
+
+    # print('Final packing: {:.1f}%'.format(100.0 * mg.pf()))
 
     return mg.r / mg.L, mg.R / mg.L
-
-
-def save(r, R, fname='out.npz'):
-    np.savez(fname, r=r, R=R)
-
-
-def sphere_plot(r, R):
-    n, dim = r.shape
-    fig = pp.figure()
-
-    if dim == 2:
-        ax = fig.gca()
-
-        for r in r:
-            ax.add_artist(pp.Circle(r, R))
-    else:
-        ax = fig.gca(projection='3d')
-
-        u = np.linspace(0, 2 * np.pi, 100)
-        v = np.linspace(0, np.pi, 100)
-        x = R * np.outer(np.cos(u), np.sin(v))
-        y = R * np.outer(np.sin(u), np.sin(v))
-        z = R * np.outer(np.ones(np.size(u)), np.cos(v))
-        for r in r:
-            # ax.plot_wireframe(r[0] + x, r[1] + y, r[2] + z, color="r")
-            ax.plot_surface(
-                r[0] + x, r[1] + y, r[2] + z,  rstride=15, cstride=15)
-
-        ax.set_zlim3d(-0.5, 0.5)
-
-    ax.set_xlim([-0.5, 0.5])
-    ax.set_ylim([-0.5, 0.5])
-    ax.set_aspect('equal')
-    pp.show()
