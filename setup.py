308a6c1322d6ec3a70728b18dab0847c3a102e72
Elliot Marsden <elliot.marsden@gmail.com>
2015-07-12 02:45:53 +0100

    Change setup script to match updated Cython.

--- setup.py
+++ setup.py
@@ -8,11 +8,17 @@ setup(
     author_email="elliot.marsden@gmail.com",
     description="Useful miscellaneous functions and classes.",
     ext_modules=cythonize([
-        Extension("field_numerics", ["field_numerics.pyx", ]),
-        Extension("walled_field_numerics", ["walled_field_numerics.pyx", ]),
-        Extension("geom_numerics", ["geom_numerics.pyx", ]),
-        Extension("distance_numerics", ["distance_numerics.pyx", ]),
-        Extension("lattice_numerics", ["lattice_numerics.pyx", ]),
+        Extension("field_numerics", ["field_numerics.pyx"],
+                  include_dirs=[numpy.get_include()]),
+        Extension("walled_field_numerics", ["walled_field_numerics.pyx"],
+                  include_dirs=[numpy.get_include()]),
+        Extension("geom_numerics", ["geom_numerics.pyx"],
+                  include_dirs=[numpy.get_include()]),
+        Extension("distance_numerics", ["distance_numerics.pyx"],
+                  include_dirs=[numpy.get_include()]),
+        Extension("lattice_numerics", ["lattice_numerics.pyx"],
+                  include_dirs=[numpy.get_include()]),
     ]),
     include_dirs=[numpy.get_include()],
 )
+
