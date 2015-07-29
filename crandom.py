824b616123ca8378e3540409a522708997368ea1
Elliot Marsden <elliot.marsden@gmail.com>
2015-07-29 21:57:43 +0100

    Add random number generation utility module.

---
+++ crandom.py
@@ -0,0 +1,8 @@
+from __future__ import print_function, division
+import numpy as np
+
+
+def randbool(n, rng=None):
+    if rng is None:
+        rng = np.random
+    return rng.randint(2, size=n) * 2 - 1
