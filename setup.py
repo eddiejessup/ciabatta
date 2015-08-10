2aeb24ae9962f52107c562490614641711a940ba
Elliot Marsden <elliot.marsden@gmail.com>
2015-08-10 22:48:56 +0100

    Try to make scipy install on travis

--- setup.py
+++ setup.py
@@ -6,6 +6,27 @@ from numpy.distutils.core import setup, Extension
 from Cython.Build import cythonize
 import numpy
 
+extensions = cython_extensions + fortran_extensions
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read().replace('.. :changelog:', '')
+
+requirements = [
+    'brewer2mpl',
+    'Cython',
+    'matplotlib',
+    'numpy',
+    'scipy',
+]
+
+test_requirements = [
+    'numpy',
+    'scipy',
+]
+
 cython_extensions = cythonize([
     Extension("ciabatta.distance_numerics",
               ["ciabatta/distance_numerics.pyx"],
@@ -36,27 +57,6 @@ fortran_extensions = [
               sources=["ciabatta/periodic_cluster.f90"]),
     Extension("ciabatta._cell_list",
               sources=cell_list_sources),
-]
-
-extensions = cython_extensions + fortran_extensions
-
-with open('README.rst') as readme_file:
-    readme = readme_file.read()
-
-with open('HISTORY.rst') as history_file:
-    history = history_file.read().replace('.. :changelog:', '')
-
-requirements = [
-    'brewer2mpl>=1.4.1',
-    'Cython>=0.23',
-    'matplotlib>=1.4.3',
-    'numpy>=1.9.2',
-    'scipy>=0.16.0',
-]
-
-test_requirements = [
-    'numpy>=1.9.2',
-    'scipy>=0.16.0',
 ]
 
 setup(
