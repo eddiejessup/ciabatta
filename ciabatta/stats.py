12c361fa93465136cbbfb8f67894bedbe9e2a8f4
Elliot Marsden <elliot.marsden@gmail.com>
2015-08-26 23:49:23 +0100

    Add stats module with function for variance of a sample variance

---
+++ ciabatta/stats.py
@@ -0,0 +1,16 @@
+"""Functions related to statistics and uncertainties"""
+from __future__ import print_function, division
+
+
+def sample_var_var(std, n):
+    """
+    The variance of the sample variance of a distribution.
+
+    Assumes the samples are normally distributed.
+
+    From: //math.stackexchange.com/q/72975
+
+    `std`: Distribution's standard deviation
+    `n`: Number of samples
+    """
+    return 2.0 * std ** 4 / (n - 1.0)
